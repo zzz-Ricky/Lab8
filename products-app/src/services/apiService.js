647eeb824b811e85a2b5b68a572092093b405bf7
zzz-Ricky <huyhhricky@gmail.com>
2024-03-13 14:37:25 -0600

    Completed editProduct

--- products-app/src/services/apiService.js
+++ products-app/src/services/apiService.js
@@ -42,4 +42,5 @@ export const addProduct = (product) => {
 // Edits an existing product by ID
 export const editProduct = (id, product) => {
     // implement it using similar logic as addProduct function but use axios.put method
+    return axios.put(`${BASE_URL}/${id}`, JSON.stringify(product));
 };
